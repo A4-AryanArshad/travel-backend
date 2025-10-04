fa8c4f0fec25bfed7438c77ad27c426b4442dd38
A4-AryanArshad <2021cs613@student.uet.edu.pk>
2025-10-04 18:17:51 +0500

    a

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const db = require('../config/db')
 
 const corsOptions = {
-  origin: ['http://localhost:5173'], // Replace with your allowed origin
+  origin: ['https://travel-five-alpha-80.vercel.app'], // Replace with your allowed origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedhallowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Allowed methods
