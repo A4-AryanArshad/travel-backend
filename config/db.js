33cb99358f27a62d69d7728f95b38980f48c89f5
A4-AryanArshad <2021cs613@student.uet.edu.pk>
2025-10-08 11:05:21 +0500

    yyyy

--- config/db.js
+++ config/db.js
@@ -3,37 +3,21 @@ const mongoose = require('mongoose');
 // Connection caching for Vercel serverless functions
 const MONGODB_URI = 'mongodb+srv://aryan:2021cs613@cluster0.xkuanbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-if (!global.mongoose) {
-  global.mongoose = { conn: null, promise: null };
-}
+
 
 const db = async () => {
-  try {
-    if (global.mongoose.conn) {
-      console.log('Using existing database connection');
-      return global.mongoose.conn;
+    try {
+
+
+        if (!MONGODB_URI) {
+            throw new Error('MONGODB_URI is not set in environment variables');
+        }
+        await mongoose.connect(MONGODB_URI).then(() => {
+            console.log('---Database Connected Successfully---');
+        })
+    } catch (error) {
+        console.log('Error while connecting to the database', error.message);
     }
-
-    if (!global.mongoose.promise) {
-      const opts = {
-        bufferCommands: false,
-        maxPoolSize: 10, // Maintain up to 10 socket connections
-        serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-        socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-      };
-
-      global.mongoose.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-        console.log('Database Connected Successfully');
-        return mongoose;
-      });
-    }
-
-    global.mongoose.conn = await global.mongoose.promise;
-    return global.mongoose.conn;
-  } catch (error) {
-    console.log('Error while connecting to the database', error.message);
-    throw error;
-  }
-};
+}
 
 module.exports = db;
