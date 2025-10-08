1e362a29fa4ee2d4b659c53230b4e0fb4f29b424
A4-AryanArshad <2021cs613@student.uet.edu.pk>
2025-10-08 09:44:56 +0500

    yyyy

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express();
-const router = require('../routes/router')
+const router = require('./routes/router')
 const cookieParser = require('cookie-parser');
-const db = require('../config/db')
+const db = require('./config/db')
 
 const corsOptions = {
-  origin: ['https://travel-five-alpha-80.vercel.app', 'http://localhost:5173', 'http://localhost:5174'], // Frontend deployed URL + localhost (5173/5174) for testing
+  origin: ['https://travel-frontend-sooty-two.vercel.app', 'http://localhost:5173', 'http://localhost:5176'], // Frontend deployed URL + localhost (5173/5174) for testing
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
